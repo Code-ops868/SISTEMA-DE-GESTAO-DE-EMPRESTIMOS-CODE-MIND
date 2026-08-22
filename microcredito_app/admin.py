61d9ba9cc3bd74e32d075f67cbcf741031f6a3f3
CODE-MIND <apoiocodeminde@gmail.com>
2026-08-22 14:32:12 +0200

    Regista model Plano no admin

--- microcredito_app/admin.py
+++ microcredito_app/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import PerfilUsuario, Cliente, Emprestimo, Parcela, Pagamento, Empresa, Notificacao, ConfiguracaoNotificacao
+from .models import PerfilUsuario, Cliente, Emprestimo, Parcela, Pagamento, Empresa, Notificacao, ConfiguracaoNotificacao, Plano
 
 admin.site.register(PerfilUsuario)
 admin.site.register(Cliente)
 admin.site.register(Emprestimo)
 admin.site.register(Parcela)
+admin.site.register(Plano)
 admin.site.register(Pagamento)
 admin.site.register(Empresa)
 admin.site.register(Notificacao)
